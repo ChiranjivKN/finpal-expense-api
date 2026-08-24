14afd3d9b3b7e0db624af8c223db9c25dfbb8ae4
Chiranjiv <analgopalwar@gmail.com>
2026-08-24 20:07:12 +0530

    feat: introduce docker-compose infrastructure

--- Finpal.Expense.Api/Program.cs
+++ Finpal.Expense.Api/Program.cs
@@ -85,10 +85,20 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FinPalDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
